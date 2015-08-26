c83b09d0c558a3a81f5d4150056817e10aced58d
Dan Jackson <e28eta@yahoo.com>
2015-08-25 22:24:22 -0700

    framework for auth

--- web.rb
+++ web.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
 
+CLOUDFLARE_EMAIL = ENV["X-Auth-Email"]
+CLOUDFLARE_AUTH = ENV["X-Auth-Key"]
+
+raise "Invalid Credentials" unless CLOUDFLARE_AUTH.to_s.length > 0 && CLOUDFLARE_EMAIL.to_s.length > 0
+
 get '/' do
-    'Hello'
+    local_auth = params["api-key"]
+    raise "Invalid api-key" unless local_auth == "foo"
+    
+    
+    
+    "Successful"
 end
